cb15630d0916e14697a80e29438bf93f33a27bd9
Mevin <mevinvencatachellum21@gmail.com>
2026-05-27 10:33:55 +0400

    add comments explaining class attributes, object, and method

--- 27 may 2026.py
+++ 27 may 2026.py
@@ -1,12 +1,12 @@
 class Student:
-    def __init__(self, name, age, programme):
-        self.name = name
+    def __init__(self, name, age, programme):     #names etc are attributes
+        self.name = name                          #self shows that name is for that specific class
         self.age = age
         self.programme = programme
 
-    def introduce(self):
+    def introduce(self):                           #introduce() is a method
         print(f" My name is {self.name}.")
         print(f"I study {self.programme}.")
 
-student1= Student("Aisha", 18, "Robotics")
+student1= Student("Aisha", 18, "Robotics")     #student1 is an object
 student1.introduce()
